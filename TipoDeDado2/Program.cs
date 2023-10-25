ed83fbb51bae52465699801ae4f0eeb2e96ecaae
caioricardoGTI <contatocaioobr@hotmail.com>
2023-10-25 19:54:15 -0300

    Adicionar arquivos de projeto.

---
+++ TipoDeDado2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Atribuindo Valore a double,float e decimal");
+Console.WriteLine();
+
+double n1 = 1.234;
+float n2 = 1.234f;
+decimal n3;
+n3 = 1.234m;
+
+Console.WriteLine(n1);
+Console.WriteLine(n2);
+Console.WriteLine(n3);
+
+Console.ReadLine();
