8d1f522f41bc5fc007bcd95e6aa4cc2a09c9e44e
caioricardoGTI <contatocaioobr@hotmail.com>
2023-10-25 20:25:11 -0300

    Aula 22

---
+++ TipoDeDados4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Definindo variaveis do tipo string e object e dynamic");
+Console.WriteLine();
+
+string nome = "Curdo C#";
+
+string titulo = "Curso C# Essencial ";
+
+Console.WriteLine(nome);
+Console.WriteLine(titulo);
+
+//Strig Builder
+
+object nota = 10;
+object valor1 = 8.44m;
+object nome1 = "maria";
+object ativa = true;
+
+Console.WriteLine(nota);
+Console.WriteLine(valor1);
+Console.WriteLine(nome1);
+Console.WriteLine(ativa);
