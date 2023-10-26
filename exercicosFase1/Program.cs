0cf768605e34fba756ceef2de771be7d566148de
caioricardoGTI <contatocaioobr@hotmail.com>
2023-10-26 08:28:10 -0300

    Ex pt 1

---
+++ exercicosFase1/Program.cs
@@ -0,0 +1,38 @@
+﻿
+// Exercícios fase 1
+// 1- Crie uma variável chamada idade e atribua a ela o valor 35 e exiba o seu valor.
+
+int idade = 35;
+
+Console.WriteLine(idade);
+
+//2- Crie uma variável chamada nome e atribua o valor "Maria" e exiba o seu valor.
+
+string nome = "Maria";
+
+Console.WriteLine(nome);
+
+//3- Crie uma variável chamada altura e atribua a ela o valor 3.45.
+
+double altura = 3.45;
+
+Console.WriteLine(altura);
+
+//4- Crie uma variável chamada data e atribua a ela a data 04/09/1999 e exiba o seu valor.
+
+DateTime data = new DateTime(1999, 09, 04);
+
+Console.WriteLine(data);
+
+//5- Crie uma constante chamada ano e atribua a ela o valor 12 e exiba o seu valor;
+
+const int ano = 12;
+
+Console.WriteLine(ano);
+
+//6 - Declare uma variável nota do tipo double como sendo do tipo Nullable type e atribua o valor 7.80 e exiba o seu valor.
+
+double? nota = 7.8;
+
+Console.WriteLine(nota);
+;
